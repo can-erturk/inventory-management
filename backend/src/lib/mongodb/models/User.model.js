a7bbd73e9daca5535f88cf5e31d4b1444a1325ce
Can Erturk <cannhann42@gmail.com>
2023-12-17 22:46:19 +0300

    Create a helper to connect to the database - Create the helper - Add mongoose model named User

---
+++ backend/src/lib/mongodb/models/User.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+const { Schema, models } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    token: {
+      type: String,
+      required: false,
+    },
+    activation: {
+      type: Object,
+      required: true,
+      default: {
+        status: false,
+        code: null,
+      },
+    },
+  },
+  { timestamps: true },
+);
+
+const User = models.User || mongoose.model('User', userSchema, 'users');
+export default User;
