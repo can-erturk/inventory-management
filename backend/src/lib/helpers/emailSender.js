13b2d3c80cf7088f06c289efbba0ac47be712df5
Can Erturk <cannhann42@gmail.com>
2023-12-19 12:38:13 +0300

    Create a helper to send emails

---
+++ backend/src/lib/helpers/emailSender.js
@@ -0,0 +1,28 @@
+import nodemailer from 'nodemailer';
+import getEnv from './getEnv.js';
+
+const SENDER_EMAIL_ADDRESS = getEnv('SENDER_EMAIL_ADDRESS');
+const SENDER_EMAIL_PASSWORD = getEnv('SENDER_EMAIL_PASSWORD');
+
+export default async function emailSender(sendTo, subject, html) {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.yandex.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: SENDER_EMAIL_ADDRESS,
+      pass: SENDER_EMAIL_PASSWORD,
+    },
+  });
+
+  try {
+    return await transporter.sendMail({
+      from: SENDER_EMAIL_ADDRESS,
+      to: sendTo,
+      subject,
+      html,
+    });
+  } catch (error) {
+    return error;
+  }
+}
