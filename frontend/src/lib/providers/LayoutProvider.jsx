0612621b4d8f6b0cb75873f16287bbeee9a9d369
Can Erturk <cannhann42@gmail.com>
2024-03-22 17:27:43 +0300

    Install Material UI and dayjs packages - npm install @mui/material @emotion/react @emotion/styled - npm install @mui/x-date-pickers - npm install dayjs

--- frontend/src/lib/providers/LayoutProvider.jsx
+++ frontend/src/lib/providers/LayoutProvider.jsx
@@ -2,10 +2,12 @@ import Footer from '@/components/layout/Footer'
 import Header from '@/components/layout/Header'
 import Sidebar from '@/components/layout/Sidebar'
 import { motion } from 'framer-motion'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 function LayoutProvider({ children }) {
   return (
-    <>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
       <motion.div initial={{ opacity: 0.5 }} animate={{ opacity: 1 }}>
         <Header />
 
@@ -14,7 +16,7 @@ function LayoutProvider({ children }) {
         <Footer />
         <Sidebar />
       </motion.div>
-    </>
+    </LocalizationProvider>
   )
 }
 
