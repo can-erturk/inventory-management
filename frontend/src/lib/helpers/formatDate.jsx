0b39c1ec160f325b10730406b0ad9b818f4104f9
Can Erturk <cannhann42@gmail.com>
2024-03-25 02:29:29 +0300

    Update the formatDate helper function - to return date or date and time at the choice

--- frontend/src/lib/helpers/formatDate.jsx
+++ frontend/src/lib/helpers/formatDate.jsx
@@ -1,11 +1,16 @@
-function formatDate(inputDate) {
+function formatDate(inputDate, isTime = false) {
   const date = new Date(inputDate)
 
   const month = ('0' + (date.getMonth() + 1)).slice(-2)
   const day = ('0' + date.getDate()).slice(-2)
   const year = date.getFullYear()
 
-  return `${month}/${day}/${year}`
+  const hours = ('0' + date.getHours()).slice(-2)
+  const minutes = ('0' + date.getMinutes()).slice(-2)
+
+  return isTime
+    ? `${month}/${day}/${year} ${hours}:${minutes}`
+    : `${month}/${day}/${year}`
 }
 
 export default formatDate
