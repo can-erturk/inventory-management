3d7ad79660a4e516f56df7cbe60136e0e778a0dc
Can Erturk <cannhann42@gmail.com>
2024-04-01 12:25:14 +0300

    Create a controller to generate a share access URL

---
+++ backend/src/app/controllers/shareAccess/generateURL.js
@@ -0,0 +1,17 @@
+import solveToken from '#helpers/solveToken.js';
+import getEnv from '#helpers/getEnv.js';
+
+const FRONTEND_BASE_URL = getEnv('FRONTEND_BASE_URL');
+
+export default async function generateURL(req, res) {
+  const { jwt } = req.body;
+
+  // Solve token
+  const user = await solveToken(jwt);
+
+  return res.send({
+    status: 200,
+    message: 'URL generated successfully.',
+    url: `${FRONTEND_BASE_URL}/share-access?id=${user.id}`,
+  });
+}
--- backend/src/config/router/shareAccess.js
+++ backend/src/config/router/shareAccess.js
@@ -3,10 +3,15 @@ import checkJwt from '#middlewares/checkJwt.js';
 import shareAccess from '#controllers/shareAccess/shareAccess.js';
 import viewShared from '#controllers/shareAccess/viewShared.js';
 import revokeAccess from '#controllers/shareAccess/revokeAccess.js';
+import generateURL from '#controllers/shareAccess/generateURL.js';
 
 const app = express();
 
 app.use(checkJwt);
+
+app.post('/generate-url', (req, res) => {
+  return generateURL(req, res);
+});
 
 app.post('/share', (req, res) => {
   return shareAccess(req, res);
