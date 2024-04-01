4b8e99254c813e83edf175bf0bf078f5587f1305
Can Erturk <cannhann42@gmail.com>
2024-04-02 02:04:27 +0300

    Update variable names and jwt's definition

--- backend/src/app/controllers/shareAccess/viewShared.js
+++ backend/src/app/controllers/shareAccess/viewShared.js
@@ -4,7 +4,7 @@ import solveToken from '#helpers/solveToken.js';
 import getUserByID from '#helpers/getUserByID.js';
 
 export default async function viewShared(req, res) {
-  const { jwt } = req.query;
+  const { jwt } = req.body;
 
   // Solve token
   const user = await solveToken(jwt);
@@ -16,26 +16,18 @@ export default async function viewShared(req, res) {
     const shared = await Product.findOne({ accessOrigin: user.id });
 
     // Filter to remove origin user
-    const sharedAccess = shared?.access.filter((id) => id !== user.id);
-
-    // If no shared access found
-    if (!sharedAccess?.length > 0) {
-      return res.send({
-        status: 404,
-        message: 'No shared access found.',
-      });
-    }
+    const sharedProducts = shared?.access.filter((id) => id !== user.id);
 
     // Create an array of shared users
-    let sharedUsers = [];
+    let sharedAccess = [];
 
     // Loop through shared access and get access shared users
-    for (let i = 0; i < sharedAccess.length; i++) {
-      const user = await getUserByID(sharedAccess[i]);
+    for (let i = 0; i < sharedProducts.length; i++) {
+      const user = await getUserByID(sharedProducts[i]);
 
-      // If user found push it to sharedUsers array
+      // If user found push it to shared array
       if (user.status) {
-        sharedUsers.push({
+        sharedAccess.push({
           id: user.data.id,
           username: user.data.username,
           email: user.data.email,
@@ -46,7 +38,7 @@ export default async function viewShared(req, res) {
     return res.send({
       status: 200,
       message: 'Shared access retrieved successfully.',
-      shared: sharedUsers,
+      shared: sharedAccess,
     });
   } catch (error) {
     return res.send({
